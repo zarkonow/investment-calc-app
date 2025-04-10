c9bec42af3fe3edad06258c4fbb2b35d48979fef
zarkonow <stojanov1987@gmail.com>
2025-04-10 18:36:07 +0200

    Add user input component with HTML and CSS styling

---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-input',
+  imports: [],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css'
+})
+export class UserInputComponent {
+
+}
