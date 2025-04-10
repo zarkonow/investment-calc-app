1ef310bf87c9c59bff0a3bb655b05f46e15b6c9e
zarkonow <stojanov1987@gmail.com>
2025-04-10 19:14:23 +0200

    Add user input component with form and data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 import { HeaderComponent } from './header/header.component';
+import { UserInputComponent } from "./user-input/user-input.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, UserInputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
 
+enteredInitialInvestment = '0';
+enteredAnnualInvestment = '0';
+enteredExpectedReturn = '5';
+enteredDuration = '10';
+
+onSubmit() {
+  console.log('submitted!');
+  
+}
 }
